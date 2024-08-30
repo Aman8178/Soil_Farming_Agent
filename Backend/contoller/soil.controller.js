bd51e41c0837aef76e885760ee981bb4514c000f
Aman8178 <211103@iiitt.ac.in>
2024-08-30 16:21:34 +0530

    Updated

--- Backend/contoller/soil.controller.js
+++ Backend/contoller/soil.controller.js
@@ -1,11 +1,11 @@
-import Soil from "../model/soil.model.js"
+import Soil from "../model/soil.model.js";
 
-export const getSoil=async(req, res)=>{
-    try{
-        const soil =await Soil.find();
-        res.status(200).json(soil)
-    }catch(error){
-        console.log("Error", error);
-        res.status(500).json(error);
+export const getSoil = async (req, res) => {
+    try {
+        const soil = await Soil.find();
+        res.status(200).json(soil);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error" });
     }
-}
+};
